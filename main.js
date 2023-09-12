104e5ad674a31a46685ebc4d2b1e265abdffb22c
Albert <albert.sparrman@gmail.com>
2023-09-12 12:57:13 +0200

    läskiga saker

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ import { script } from './script.js'
 
 document.querySelector('#app').innerHTML = `
     <div id=answerScreen>
-        <h1 id="answer">Korrekt</h1>
+        <h1 id="answer">Abloot</h1>
         <p id="subans">Wow du vet verkligen mycket om herr Abl</p>
-        <button id="nextQuestion">Nästa fråga</button>
+        <button id="nextQuestion">Starta quiz</button>
     </div>
     <h2 id="questionText"></h2>
-    <h2 id="timer"></h2>
+    <h3 id="score"></h3>
+    <h3 id="timer"></h3>
     <button id="one">1</button>
     <button id="two">2</button>
     <button id="three">3</button>
--- script.js
+++ script.js
@@ -9,10 +9,15 @@ export function script(element) {
     const answerScreen = element.querySelector('#answerScreen')
     const nextQuestion = element.querySelector('#nextQuestion')    
     
-    let score = 0
+
+    let scoreMultiplier = 1
+    let Score = 0
+    let currentScore = 0
 
     
     function correct() {
+        Score = 10
+        console.log(Score)
         answerScreen.style.display = "flex";
         answerScreen.style.backgroundColor = "rgb(31, 172, 62)";
         answer.textContent = 'Du har rätt'
@@ -20,6 +25,8 @@ export function script(element) {
     } 
     
     function wrong() {
+        Score = 0
+        console.log(Score)
         answerScreen.style.display = "flex";
         answerScreen.style.backgroundColor = "rgb(231, 10, 30)";
         answer.textContent = 'Du har fel'
@@ -28,7 +35,7 @@ export function script(element) {
 
     function end() {
         answerScreen.style.display = "flex";
-        answerScreen.style.backgroundColor = "rgb(231, 10, 30)";
+        answerScreen.style.backgroundColor = "rgb(35, 21, 97)";
         answer.textContent = 'slut'
         subans.textContent = 'Hur kunde du missa denna viktiga fakta om Ablet'
     }
@@ -53,102 +60,104 @@ export function script(element) {
 
       let b = 0 
 
+      nextQuestion.addEventListener('click', (e)=> {
+        e.preventDefault()
+        newQuestion()           
+      })
+
+
       function newQuestion(){ 
         if(b < options.q.length){
         answerScreen.style.display = "none"; 
+    
+        let answer = options.q[b].answer
+        let potAns = options.q[b].potentialAnswers
+        questionText.innerHTML = options.q[b].question
 
-            let answer = options.q[b].answer
-            let potAns = options.q[b].potentialAnswers
-            questionText.innerHTML = options.q[b].question
-            let timeLeft = 9
-            timer.textContent = 10  
-            let timerId = setInterval(countdown, 1000);
+        let timeLeft = 9
+        timer.textContent = 10  
+        let timerId = setInterval(countdown, 1000);
 
-            function countdown() {
-                if (timeLeft == 0) {
-                    clearInterval(timerId);
-                    wrong()
-                } else {
-                    timer.textContent = timeLeft
-                    timeLeft--;
-                    console.log(timeLeft)
-                }
+
+        function countdown() {
+            if (timeLeft == 0) {
+                clearInterval(timerId);
+                wrong()
+            } else {
+                timer.textContent = timeLeft
+                timeLeft--;
+            }
                 
-              }              
-              b+=1
+        }              
+            
+            
+        b+=1
               
 
-            shuffle(potAns)            
+        shuffle(potAns)            
 
-            ans1.innerHTML = potAns[0]
-            ans2.innerHTML = potAns[1]
-            ans3.innerHTML = potAns[2]
-            ans4.innerHTML = potAns[3]
+        ans1.innerHTML = potAns[0]
+        ans2.innerHTML = potAns[1]
+        ans3.innerHTML = potAns[2]
+        ans4.innerHTML = potAns[3]
+            
+            
+        ans1.addEventListener('click', (e)=> {
+            e.preventDefault()
+            if(potAns[0]==answer){
+                clearInterval(timerId)
+                correct()
+            }
+            else {
+                clearInterval(timerId)
+                wrong()
+            }
+        })
             
         
-            ans1.addEventListener('click', (e)=> {
-                e.preventDefault()
-                if(potAns[0]==answer){
-                    clearInterval(timerId);
-                    correct()
-                    score += 10
-                }
-                else {
-                    clearInterval(timerId);
-                    wrong()
-                }
-            })
-        
-            ans2.addEventListener('click', (e)=> {
-                e.preventDefault()
-                if(potAns[1]==answer){
-                    clearInterval(timerId);
-                    correct()
-                    score += 10
-                }
-                else {
-                    clearInterval(timerId);
-                    wrong()
-                }
-            })
-        
-            ans3.addEventListener('click', (e)=> {
-                e.preventDefault()
-                if(potAns[2]==answer){
-                    clearInterval(timerId);
-                    correct()
-                    score += 10
-                }
-                else {
-                    clearInterval(timerId);
-                    wrong()
-                    console.log(answers.some(r=> potAns[2].includes(r)))
-                }
-            })
-        
-            ans4.addEventListener('click', (e)=> {
-                e.preventDefault()
-                if(potAns[3]==answer){
-                    clearInterval(timerId);
-                    correct()
-                    score += 10
-                }
-                else {
-                    clearInterval(timerId);
-                    wrong()
-                }
-            }) 
-        }
-        else{
-            end()
-        } 
-        }
-        
-
-        nextQuestion.addEventListener('click', (e)=> {
+        ans2.addEventListener('click', (e)=> {
             e.preventDefault()
-            newQuestion()           
+            if(potAns[1]==answer){
+                clearInterval(timerId)
+                correct()
+            }
+            else {
+                clearInterval(timerId)
+                wrong()
+            }
         })
         
+        ans3.addEventListener('click', (e)=> {
+            e.preventDefault()
+            if(potAns[2]==answer){
+                clearInterval(timerId)
+                correct()
+            }
+            else {
+                clearInterval(timerId)
+                wrong()
+            }
+        })
+        
+        ans4.addEventListener('click', (e)=> {
+            e.preventDefault()
+            if(potAns[3]==answer){
+                clearInterval(timerId)
+                correct()
+            }
+            else {
+                clearInterval(timerId)
+                wrong()
+            }
+        }) 
+        
+        currentScore += Score
+        score.textContent = currentScore 
+        console.log(potAns[0] ==answer)
+        console.log(Score)
+    }
+    else{
             
+    } 
+}           
 }
